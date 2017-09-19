39a0475340ea7870696c62e21e51eec75ecf7d2c
susiyanti <susiyanti.g11@gmail.com>
2017-09-19 12:34:57 +0700

    Fix crashing when back, and exo player resume after background

--- app/src/main/java/com/example/susiyanti/bakingapp/RecipeFragment.java
+++ app/src/main/java/com/example/susiyanti/bakingapp/RecipeFragment.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.susiyanti.bakingapp.model.Recipe;
 import com.example.susiyanti.bakingapp.retrofit.RecipeService;
@@ -78,6 +79,7 @@ public class RecipeFragment extends Fragment {
             @Override
             public void onFailure(Call<ArrayList<Recipe>> call, Throwable t) {
                 Log.v("http fail: ", t.getMessage());
+                Toast.makeText(getContext(),"Failed to retrieve Recipe", Toast.LENGTH_SHORT).show();
             }
         });
 
